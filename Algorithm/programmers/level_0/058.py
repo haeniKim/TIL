e6ecbe72e1ec9b8e46b25991495fbbb960b4734c
haeniKim <aa01086498026@gmail.com>
2023-02-15 23:55:42 +0900

    Add 230215 practice

--- Algorithm/programmers/level_0/058.py
+++ Algorithm/programmers/level_0/058.py
@@ -1,1 +1,6 @@
 #인덱스 바꾸기
+def solution(my_string, num1, num2):
+    list_ms = list(my_string)
+    list_ms[num1],list_ms[num2] = list_ms[num2],list_ms[num1]
+    answer = ''.join(list_ms)
+    return answer
--- Algorithm/programmers/level_0/060.py
+++ Algorithm/programmers/level_0/060.py
@@ -1,1 +1,4 @@
 #약수 구하기
+def solution(n):
+    answer = [i for i in range(1,n+1) if n % i == 0]
+    return answer
