1a5c71e90060837500d9b5ea951ed55436e3e916
haeniKim <aa01086498026@gmail.com>
2023-02-21 23:03:48 +0900

    Add 230221 practice

--- Algorithm/programmers/level_0/079.py
+++ Algorithm/programmers/level_0/079.py
@@ -1,1 +1,14 @@
-#최댓값 만들기 (2)
+#최댓값 만들기 (2) 
+#중첩 for문 이용, 두 원소 곱들을 모두 구한 리스트에서 최대값
+def solution(numbers):
+    mul = []
+    for i in range(len(numbers)-1):
+        for j in range(i+1,len(numbers)):
+            mul.append(numbers[i] * numbers[j])
+    return max(mul)
+
+#다른 풀이
+#단순하게 음수의 경우 정렬 후 가장 작은 값 두개의 곱으로 최대값을 판정할 수 있음
+def solution2(numbers):
+    numbers = sorted(numbers)
+    return max(numbers[0] * numbers[1], numbers[-1]*numbers[-2]) 
--- Algorithm/programmers/level_0/081.py
+++ Algorithm/programmers/level_0/081.py
@@ -1,1 +1,12 @@
 #숨어있는 숫자의 덧셈 (2)
+def solution(my_string):
+    for i in my_string:
+        if i.isalpha():
+            my_string = my_string.replace(i,' ')
+    num_list = map(int, my_string.split())
+    return sum(num_list)
+
+#다른 사람의 풀이 - 더 깔끔하게 
+def solution2(my_string):
+    s = ''.join(i if i.isdigit() else ' ' for i in my_string)
+    return sum(int(i) for i in s.split())
