21f63d79828a831cb40e2e288e29a44078608bff
Haeni <haeng.gu723@gmail.com>
2024-01-08 15:52:03 +0900

    240108 django self study

--- Django/django_self1/pybo/views.py
+++ Django/django_self1/pybo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
-#from django.http import HttpResponse
+from .forms import QuestionForm, AnswerForm
+from django.http import HttpResponseNotAllowed
 
 # Create your views here.
 
@@ -19,3 +20,34 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST': # 저장하기 
+        form = QuestionForm(request.POST)
+        if form.is_valid(): # 폼이 유효하면
+            question = form.save(commit=False) # 임시 저장해 question 객체 리턴
+            question.create_date = timezone.now() # 실제 저장을 위해 작성일시 설정
+            question.save() # 데이터를 실제로 저장
+            return redirect('pybo:index')
+    else: # 질문 등록하기
+        form = QuestionForm()
+    context = {'form':form}
+    return render(request, 'pybo/question_form.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
