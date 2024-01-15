ff0501a22d78703bdecac0688de2602cd55accae
Haeni <haeng.gu723@gmail.com>
2024-01-15 23:15:14 +0900

    240115 django study

--- Django/django_prac1/common/views.py
+++ Django/django_prac1/common/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
 
 # Create your views here.
