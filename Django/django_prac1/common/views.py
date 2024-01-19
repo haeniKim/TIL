cc51b11b72acb0d9743435b7833ecbdc13e61ea6
Haeni <haeng.gu723@gmail.com>
2024-01-19 17:50:33 +0900

    240119 django study

--- Django/django_prac1/common/views.py
+++ Django/django_prac1/common/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from common.forms import UserForm
 
 def logout_view(request):
@@ -13,9 +13,9 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password) #사용자 인증
-            login(request, user) #로그인
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
             return redirect('index')
-        else:
-            form = UserForm()
-        return render(request, 'common/signup.html', {'form':form})
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
--- Django/django_prac1/pybo/views.py
+++ Django/django_prac1/pybo/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm 
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -19,26 +20,30 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST': # 저장하기 
         form = QuestionForm(request.POST)
         if form.is_valid(): # 폼이 유효하면
             question = form.save(commit=False) # 임시 저장해 question 객체 리턴
+            question.author = request.user # author 속성에 로그인 계정 저장
             question.create_date = timezone.now() # 실제 저장을 위해 작성일시 설정
             question.save() # 데이터를 실제로 저장
             return redirect('pybo:index')
