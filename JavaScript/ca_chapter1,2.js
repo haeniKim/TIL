d2398f122f1e8102fb509acf2c4239cc2fb7bd16
Haeni <haeng.gu723@gmail.com>
2024-01-29 17:57:39 +0900

    240129 JS study

---
+++ JavaScript/ca_chapter1,2.js
@@ -0,0 +1,42 @@
+// chapter 1
+name = "Mike";
+age = 30;
+
+// alert(name);
+
+console.log(age);
+
+// -------------------------
+// chapter 2
+const message = "I'm a boy";
+const message2 = 'I\'m a boy';
+
+const message3 = `My name is ${name}`;
+
+const message4 = `나는 ${30+3}살 입니다.`;
+console.log(message4)
+
+const x = 1/0;
+console.log(x)
+
+const name2 = "Mike";
+const y = name2/2;
+
+console.log(y)
+
+const a = true;
+console.log(name == 'mike')
+
+// undefined
+let age2;
+console.log(age2)
+let user = null;
+
+// typeof 연산자
+console.log(typeof 3);
+console.log(typeof name);
+console.log(typeof null);
+
+// 숫자형 + 문자형
+console.log("내 나이는 "+ age + "살");
+
---
+++ JavaScript/ca_chapter3,4.js
@@ -0,0 +1,14 @@
+// chapter 3
+//const  name = prompt("이름을 입력하세요.");
+//alert(`환영합니다 ${name} 님 :)` );
+
+//const isAdult = confirm("당신은 성인 입니까?");
+// 확인 -> true, 취소 -> false
+//console.log(isAdult)
+
+// -------------------------
+// chapter 4
+console.log(
+    Number(true),
+    Number(false)
+)
