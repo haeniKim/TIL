49170e6310eca9a01b6e3c440bc3d980a853e9df
Haeni <haeng.gu723@gmail.com>
2024-02-01 17:04:18 +0900

    new key

--- Django/django_prac1/config/settings.py
+++ Django/django_prac1/config/settings.py
@@ -20,7 +20,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7q4dg-rsrqhf#x=ug^8(8u+7wrqohd2uq2)$-oboott##nu42^'
+# SECRET_KEY = 'django-insecure-7q4dg-rsrqhf#x=ug^8(8u+7wrqohd2uq2)$-oboott##nu42^'
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
